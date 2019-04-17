20bdf6cf42ff091d970f8bf7985013df6bc6b3ff
MohamedAbdelsalam9 <mohamedashraf9412@gmail.com>
2019-04-17 19:39:22 -0400

    Update load_dataset

--- util.py
+++ util.py
@@ -15,7 +15,7 @@ from contextlib import redirect_stdout
 from math import ceil
 
 #Load Dataset
-def load_data(train_path=None, test_path=None, val_path=None, batch_size=32, train_val_ratio=0.8):
+def load_data(train_path, test_path, val_path=None, batch_size=32, train_val_ratio=0.8):
     with open(train_path) as f:
         lines = f.readlines()
     x_train = np.array([[np.float32(i) for i in line.split(' ')] for line in lines])
@@ -37,23 +37,18 @@ def load_data(train_path=None, test_path=None, val_path=None, batch_size=32, tra
         x_val = x_val.reshape((x_val.shape[0], 1, 28, 28))
         y_val= np.zeros((x_val.shape[0], 1))
         validation = data.TensorDataset(torch.from_numpy(x_val).float(), torch.from_numpy(y_val))
-        train_loader = data.DataLoader(train, batch_size=batch_size, shuffle=True)
-        val_loader = data.DataLoader(validation, batch_size=batch_size, shuffle=False)
-        test_loader = data.DataLoader(test, batch_size=batch_size, shuffle=False)
     else:
-        #Create a validation set
+        #Create a validation set from training set with ratio (1-train_val_ratio)
         data_len = len(train)
-        train_len = np.round(data_len * train_val_ratio)
+        train_len = int(data_len * train_val_ratio)
         indices = np.arange(data_len)
         indices = np.random.permutation(indices)
         train_indices = indices[:train_len]
         val_indices = indices[train_len:]
-
         validation = train[val_indices]
         train = train[train_indices]
 
-        train_loader = data.DataLoader(train, batch_size=batch_size, shuffle=True)
-        val_loader = data.DataLoader(validation, batch_size=batch_size, shuffle=False)
-        test_loader = data.DataLoader(test, batch_size=batch_size, shuffle=False)
-
+    train_loader = data.DataLoader(train, batch_size=batch_size, shuffle=True)
+    val_loader = data.DataLoader(validation, batch_size=batch_size, shuffle=False)
+    test_loader = data.DataLoader(test, batch_size=batch_size, shuffle=False)
     return (train_loader, val_loader, test_loader)
